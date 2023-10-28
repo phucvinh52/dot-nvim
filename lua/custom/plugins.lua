0b8f96537b35cb03cfe910a508268bd09e509d35
Fx Phúc Vinh Fx <kiemsi.lv@gmail.com>
2023-10-28 13:58:45 +0700

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -128,12 +128,50 @@ local plugins = {
     "sheerun/vim-polyglot"
   },
   {
+    "kevinhwang91/nvim-hlslens",
+    event = "BufReadPost",
+    config = function()
+      require("scrollbar.handlers.search").setup {
+        {
+          nearest_float_when = false,
+          override_lens = function(render, posList, nearest, idx, relIdx)
+            local sfw = vim.v.searchforward == 1
+            local indicator, text, chunks
+            local absRelIdx = math.abs(relIdx)
+            if absRelIdx > 1 then
+              indicator = ("%d%s"):format(absRelIdx, sfw ~= (relIdx > 1) and icons.misc.up or icons.misc.down)
+            elseif absRelIdx == 1 then
+              indicator = sfw ~= (relIdx == 1) and icons.misc.up or icons.misc.down
+            else
+              indicator = icons.misc.dot
+            end
+            local lnum, col = unpack(posList[idx])
+            if nearest then
+              local cnt = #posList
+              if indicator ~= "" then
+                text = ("[%s %d/%d]"):format(indicator, idx, cnt)
+              else
+                text = ("[%d/%d]"):format(idx, cnt)
+              end
+              chunks = { { " ", "Ignore" }, { text, "HlSearchLensNear" } }
+            else
+              text = ("[%s %d]"):format(indicator, idx)
+              chunks = { { " ", "Ignore" }, { text, "HlSearchLens" } }
+            end
+            render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+          end,
+        },
+      }
+    end,
+  },
+  {
     "petertriho/nvim-scrollbar",
     event = "WinScrolled",
     config = function()
       require "custom.configs.scrollbar"
     end,
   },
+  
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
