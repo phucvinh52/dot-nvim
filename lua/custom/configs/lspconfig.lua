44295d3f5f842b8adce0d483b9d1736e491463d6
Fx Phúc Vinh Fx <kiemsi.lv@gmail.com>
2023-12-23 22:39:00 +0700

    Update lspconfig.lua

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "solidity_ls_nomicfoundation" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "solidity_ls_nomicfoundation", "html }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
